8c0bd8534d5569050f8f432bbba3aaa894d95dbc
Piotr Lada <sobieskiandy@gmail.com>
2021-12-19 22:54:47 +0100

    Próbowałem napisać dość prosty kod, ale jestem zmęczony. Dobranoc

--- main/main.cpp
+++ main/main.cpp
@@ -6,6 +6,13 @@ int main()
 {
     cout << "Hello world!" << endl;
     int a=44, b=55;
-    cout<<a+b;
+    cout<<a+b<<endl;
+    while(a<b)
+    {
+        int c=0;
+
+        cout<<"<<"<<endl;
+        a+=1;
+    }
     return 0;
 }
